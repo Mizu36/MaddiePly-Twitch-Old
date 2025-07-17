3a2fdfb1a9fd522d1a55148acba0cd7aae3f5d4a
Mizu36 <cealtizer@gmail.com>
2025-07-17 16:23:01 -0700

    Added DEBUG

--- bot_utils.py
+++ bot_utils.py
@@ -1,4 +1,5 @@
 shared_bot = None
+DEBUG = None
 
 def set_bot_instance(bot_instance):
     global shared_bot
@@ -6,3 +7,9 @@ def set_bot_instance(bot_instance):
 
 def get_bot_instance():
     return shared_bot
+
+def get_debug():
+    return DEBUG
+    
+def set_debug(param):
+    DEBUG = param
