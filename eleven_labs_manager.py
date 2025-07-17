488a43ab1a4276b8e35d0b1842bda7c02b9cdbff
Mizu36 <cealtizer@gmail.com>
2025-07-17 16:24:36 -0700

    Fixed code that broke from Elevenlabs api update

--- eleven_labs_manager.py
+++ eleven_labs_manager.py
@@ -1,6 +1,8 @@
-from elevenlabs import stream, play, save
-from elevenlabs.client import ElevenLabs, VoiceSettings
+from elevenlabs import stream, play, save, VoiceSettings
+from elevenlabs.client import ElevenLabs
 from dotenv import load_dotenv
+from bot_utils import DEBUG
+from json_manager import load_settings
 import time
 import os
 
@@ -11,11 +13,12 @@ try:
 except TypeError:
   exit("Ooops! You forgot to set ELEVENLABS_API_KEY in your environment!")
 
-DEFAULT_VOICE = "Aria"
-MODEL = "eleven_flash_v2_5"
+DEFAULT_VOICE = "9BWtsMINqrJLrRacOk9x" #Aria
 DEFAULT_STABILITY = 0.5 #Ranges 0 to 1
 DEFAULT_SPEED = 1 #Ranges 0.7 to 1.2
 DEFAULT_SIMILARITY = 0.75 #Ranges 0 to 1
+
+MODELS = ["eleven_v3", "eleven_multilingual_v2", "eleven_flash_v2_5", "eleven_flash_v2", "eleven_turbo_v2_5", "eleven_turbo_v2"]
 
 class ElevenLabsManager:
 
@@ -25,43 +28,40 @@ class ElevenLabsManager:
         #print(f"\nAll ElevenLabs voices: \n{all_voices}\n")
 
     # Convert text to speech, then save it to file. Returns the file path
-    def text_to_audio(self, input_text, voice=DEFAULT_VOICE, save_as_wave=True, subdirectory=""):
-        client.voices.edit_settings(
-           voice_id=voice,
-           request=VoiceSettings(
+    def text_to_audio(self, input_text, voice=DEFAULT_VOICE, save_as_wave=True, subdirectory="audio", model="eleven_multilingual_v2"):
+        audio_saved = client.text_to_speech.convert(
+          text=input_text,
+          voice_id=voice,
+          model_id=model,
+          voice_settings=VoiceSettings(
               stability=DEFAULT_STABILITY,
               similarity_boost=DEFAULT_SIMILARITY,
               speed=DEFAULT_SPEED
            )
-        )
-        audio_saved = client.generate(
-          text=input_text,
-          voice=voice,
-          model=MODEL
         )
         if save_as_wave:
           file_name = f"___Msg{str(hash(input_text))}.wav"
         else:
           file_name = f"___Msg{str(hash(input_text))}.mp3"
         tts_file = os.path.join(os.path.abspath(os.curdir), subdirectory, file_name)
-        save(audio_saved,tts_file)
+        save(audio_saved, tts_file)
         return tts_file
 
     # Convert text to speech, then play it out loud
-    def text_to_audio_played(self, input_text, voice=DEFAULT_VOICE):
+    def text_to_audio_played(self, input_text, voice=DEFAULT_VOICE, model="eleven_multilingual_v2"):
         audio = client.generate(
           text=input_text,
           voice=voice,
-          model=MODEL
+          model=model
         )
         play(audio)
 
     # Convert text to speech, then stream it out loud (don't need to wait for full speech to finish)
-    def text_to_audio_streamed(self, input_text, voice=DEFAULT_VOICE):
+    def text_to_audio_streamed(self, input_text, voice=DEFAULT_VOICE, model="eleven_multilingual_v2"):
         audio_stream = client.generate(
           text=input_text,
           voice=voice,
-          model=MODEL,
+          model=model,
           stream=True
         )
         stream(audio_stream)
